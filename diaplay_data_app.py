60c15843e2de544011ef418ccea70d3cde8e3fa3
刘啸宇 <yujie22@163.com>
2023-10-11 18:12:42 +0800

    Update diaplay_data_app.py

--- diaplay_data_app.py
+++ diaplay_data_app.py
@@ -13,4 +13,4 @@ def visualize_data(gender):
 markdown_txt = gr.Markdown("## 选择**性别**，查看小学四年级学生体测数据分布！")
 gender_choice = gr.Radio(["男生", "女生"], label="性别", info="请选择性别")
 
-gr.Interface(fn=visualize_data, inputs=gender_choice, outputs="image").launch()
+gr.Interface(fn=visualize_data, inputs=[markdown_txt, gender_choice], outputs="image").launch()
