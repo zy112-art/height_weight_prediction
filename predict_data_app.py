613ae410ba8ca5baeb0899ffe11770b4e9ce8534
刘啸宇 <yujie22@163.com>
2023-10-11 18:01:57 +0800

    Update predict_data_app.py

--- predict_data_app.py
+++ predict_data_app.py
@@ -10,8 +10,9 @@ def predict_and_visualize_data(height, weight, gender):
     return 'scatter_plot_2.png'
 
 # 创建 Gradio 接口
+markdown_txt = gr.Markdown("## 输入自己测量的**身高**、**体重**数据，尝试让人工智能模型来帮你**预测**对应水平等级吧！")
 height = gr.Slider(50, 250, value=160.2, label="身高（厘米）", info="请选择介于50到250之间的数值")
 weight = gr.Slider(10, 200, value=44.1, label="体重（千克）", info="请选择介于10到200之间的数值")
 gender_choice = gr.Radio(["男生", "女生"], label="性别", info="请选择性别")
 
-gr.Interface(fn=predict_and_visualize_data, inputs=[height, weight, gender_choice], outputs="image").launch()
+gr.Interface(fn=predict_and_visualize_data, inputs=[markdown_txt, height, weight, gender_choice], outputs="image").launch()
