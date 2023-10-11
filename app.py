f838ba31aff409c8c304fdf6c03c64c9f97ecd86
刘啸宇 <yujie22@163.com>
2023-10-11 15:17:55 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,11 @@ def visualize_data(gender):
         read_visualize_data("男生")
     elif gender == "女生":
         read_visualize_data("女生")
+    return 'scatter_plot_1.png'
+
+# # 第二个交互
+# def predict_and_visualize_data(height, weight, gender):
+#     return predict_data_visualize(gender, height, weight)
 
 iface = gr.Interface(
     fn=visualize_data,
@@ -15,3 +20,10 @@ iface = gr.Interface(
 )
 iface.launch()
 
+# iface2 = gr.Interface(
+#     fn=predict_and_visualize_data,
+#     inputs=[gr.inputs.Number(label="身高 (cm)"), gr.inputs.Number(label="体重 (kg)"),
+#             gr.inputs.Dropdown(['男生', '女生'], label="性别")],
+#     outputs="auto"
+# )
+# iface2.launch()
