4a2924f066a3caa6ad5a578999f7981be304c898
刘啸宇 <yujie22@163.com>
2023-10-16 15:09:15 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -37,7 +37,7 @@ def read_visualize_data(gender):
         level_data = X[y == level]
         plt.scatter(level_data['体重（千克）'], level_data['身高（厘米）'], c=colors[level], marker=markers[level], label=level, s=90)
     
-    plt.legend(fontproperties=custom_font) # 添加图例
+    plt.legend() # 添加图例
     plt.xlabel(X.columns[0],fontsize=20,loc='center', fontproperties=custom_font)
     plt.ylabel(X.columns[1],fontsize=20,loc='center', fontproperties=custom_font)
     plt.title('散点图',fontsize=20,loc='center')
@@ -97,7 +97,7 @@ def predict_data_visualize(gender, height, weight, K=5):
         level_data = X[y == level]
         plt.scatter(level_data['体重（千克）'], level_data['身高（厘米）'], c=colors[level], marker=markers[level], label=level, s=90)
     
-    plt.legend(fontproperties=custom_font) # 添加图例
+    plt.legend() # 添加图例
     # y = y.values.tolist()
     # c = []
     # for i in y:
